845c544e4c36801bf519f3d409bc69436c8ed658
H3neique.jpeg <cc25326@g.unicamp.br>
2025-10-18 20:19:36 -0300

    POST LANÇADO (V.1)

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,11 +1,42 @@
 async function cadastrarClientes(){
-    const nome =  document.getElementById('').value
-    const email =  document.getElementById('').value
-    const dataNasc =  document.getElementById('').value
-    const endereco =  document.getElementById('').value
-    const senha =  document.getElementById('').value
+    const nome =  document.getElementById('nome').value
+    const email =  document.getElementById('email').value
+    const dataNasc =  document.getElementById('idade').value
+    const endereco =  document.getElementById('endereco').value
+    const senha =  document.getElementById('senha').value
+    const Csenha =  document.getElementById('confirmar-senha').value
 
     const cadastro = {
+        nome: nome,
+        email: email,
+        dataNasc: dataNasc,
+        endereco: endereco,
+        senha: senha
+    }
 
+    if (senha !== Csenha){
+        alert("As senhas não coincidem")
+    }
+   try{
+    const response = await fetch('http://localhost:8090/daroca/clientes',{
+        method: "POST",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body: JSON.stringify(cadastro)
+    })
+   if (!response.ok) {
+            throw new Error("Erro ao cadastrar cliente")
+        }
+
+        const data = await response.json()
+        
+        console.log("Cliente cadastrado com sucesso:", data)
+        alert("Cadastro realizado com sucesso!")
+
+    } 
+    catch (error) {
+        console.error(error)
+        alert("Não foi possível realizar o cadastro.")
     }
 }
