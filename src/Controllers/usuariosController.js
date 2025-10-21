f7e4eb006df72bebb4923e42b34698b5f8f8e89f
Daniel Bisco <cc25560@g.unicamp.br>
2025-10-21 16:23:02 -0300

    usuariosController.js
    Eu criei o arquivo usuariosController.js e implementei o login, onde o usuário deverá passar o username e senha e os mesmos serão validados com as informações armazenadas
    na tabela USUARIO no banco de dados.
    A função login tratará a requisição do tipo POST que receberá o username e senha que
    o usuário deverá digitar no front-end. Os dados serão comparados com os armazenados no banco de dados. Se os dados estiverem corretos será retornado o token através do comando res.json({ token });
    O token será gerado através da função jwt.sign(). No token é possível acrescentar as informações que desejamos checar enquanto o usuário estiver logado. Podemos definir
    algumas configurações, por exemplo, por quanto tempo o token será válido.
    A função inserir será usada para quando o usuário resolver se registrar.

--- src/Controllers/usuariosController.js
+++ src/Controllers/usuariosController.js
@@ -4,6 +4,34 @@ const Usuario = require("../models/usuairo");
 
 
 require("dotenv").config();
-const SECRET = process.env.SECRET 
+const SECRET = process.env.SECRET
 
-as
+async function login(req, res) {
+    try {
+        const { username, senha } = req.body;
+        const usuario = await Usuario.buscar(username);
+
+        if (!usuario) return res.status(401).json({ msg: "Credenciais inválidas" });
+
+        const senhaValida = await bcrypt.compare(senha, usuario.senha);
+        if (!senhaValida) return res.status(401).json({msg: "Credenciais inválidas"});
+
+
+        const token = jwt.sign({id: usuario.id, username: usuario.username, 
+            role: usuario.role}, SECRET, {expiresIn: "1h"});
+            res.json({token});
+    }   catch (err){
+        res.status(500).json({erro: err.message});
+    }
+}
+
+async function inserir(req, res) {
+    try{
+        const resultado = await Usuario.inserir(req.body);
+        res.status(201).json(resultado);
+    } catch (err) {
+        res.status(500).json({erro: err.message});
+    }
+}
+
+    module.exports = {login, inserir};
