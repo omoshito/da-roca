8b484332f3f31a7e07c4330b65cdeadf6c339199
H3neique.jpeg <cc25326@g.unicamp.br>
2025-10-20 21:58:51 -0300

    Inicio da adição do token de autenticação

--- src/models/daroca.js
+++ src/models/daroca.js
@@ -5,30 +5,6 @@ async function listarProdutos(){
     return dados.recordset
 }
 
-async function cadastrarClientes(cliente){
-    const {cpf, nome, telefone, endereco, login, senha} = cliente
-    try{
-        const request = new mssql.Request()
-        
-        request.input('cpfCliente',VarChar(11), cpf)
-        request.input('nomeCliente', mssql.VarChar(100), nome)
-        request.input('telefoneCliente', mssql.VarChar(9), telefone)
-        request.input('enderecoCliente', mssql.VarChar(50), endereco)
-        request.input('loginCliente', mssql.VarChar(80), login)
-        request.input('senhaCliente', mssql.VarChar(20), senha)
-
-        const query = `INSERT INTO daroca.clientes (cpf, nome, telefone, email, senha, endereco) VALUES (@cpfCliente, @nomeClientem 
-                       @telefoneCliente, @enderecoCliente, @loginCliente, @senhaCliente)`
-
-        await request.query(query)
-
-        return{mensagem:"Cliente inserido com sucesso"}
-
-    }
-    catch(error){
-        return {mensagem : "Não foi possível cadastrar o cliente", erro : error}
-    }
-}
 
 async function excluirCliente(CPF, cliente){
     const {cpf ,login, senha} = cliente
@@ -76,7 +52,7 @@ async function atualizarCliente(cliente){
     }
     catch(error){
         return {mensagem : "Não foi possivel atualizar os dados do cliente"}
-    }
+    } 
 }
 
-module.exports = {listarProdutos , cadastrarClientes, excluirCliente, atualizarCliente}
+module.exports = {listarProdutos , excluirCliente, atualizarCliente}
